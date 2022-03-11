4fc74e75a942a448fb5e35355a0389b667b20bfa
jorge-ordering <jorge@ordering.co>
2022-03-10 21:53:56 -0500

    get_users

--- main.py
+++ main.py
@@ -112,7 +112,22 @@ def login():
     tags=["Users"]
 )
 def get_users():
-    pass
+    """
+        This path operation shows all users in the app
+
+        Parameters:
+            -
+
+        Returns a json list with all users in the app, with the following keys:
+            - user_id: UUID
+            - email: Emailstr
+            - first_name: str
+            - last_name: str
+            - birth_date: datetime
+    """
+    with open("users.json", "r", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
 
 @app.get(
     path="/users/{user_id}",
