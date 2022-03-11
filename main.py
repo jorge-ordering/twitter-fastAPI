4c6039ad5ea9de60175205c89543ce0a4fd70aa5
jorge-ordering <jorge@ordering.co>
2022-03-10 20:31:55 -0500

    model_tweet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from datetime import date
+from datetime import date, datetime
+from importlib.resources import contents
 from typing import Optional
 from uuid import UUID
 
@@ -18,7 +19,8 @@ class UserBase(BaseModel):
 class UserLogin(UserBase):
     password: str = Field(
         ...,
-        min_length=8
+        min_length=8,
+        max_length=64
     )
 
 class User(UserBase):
@@ -29,7 +31,15 @@ class User(UserBase):
     )
     birth_date: Optional[date] = Field(default=None)
 class Tweet(BaseModel):
-    pass
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ...,
+        min_length=1,
+        max_length=256
+    )
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
 
 @app.get(path="/")
 def home():
